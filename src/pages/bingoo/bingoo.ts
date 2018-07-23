b026d4aee0ab78ea6c8f7255d549700253fb649c
Isaias Santos <isaias.santos@veus.com.br>
2018-07-23 09:58:18 -0300

    Alterações projeto

--- src/pages/bingoo/bingoo.ts
+++ src/pages/bingoo/bingoo.ts
@@ -15,11 +15,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BingooPage {
 
+  public sorteado: any;
+  public ultimos: any;
+  public todos: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.ultimos = [];
+    this.todos = [];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BingooPage');
+  }
+
+  sorteia(){
+    this.sorteado = Math.floor(Math.random()*90)+1;
+    this.insereUltimo();
+  }
+
+  insereUltimo(){
+    this.ultimos.push(this.sorteado);
+    if(this.ultimos.length > 8){
+      this.ultimos.splice(0, 1);
+    }
+  }
+
+  iniciaTodos(){
+    for(let i = 0; i < 90; i++){
+      this.todos.push(0);
+    }
   }
 
 }
